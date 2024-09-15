31701754db54339a98e9b31a21f1fc1982ad6c55
Hyago Alves <dev.hyago@outlook.com>
2024-09-15 15:03:28 -0300

    feat: category domain class test

--- src/main/java/dev/hyago/ecommerce_spring_boot/controllers/CategoryController.java
+++ src/main/java/dev/hyago/ecommerce_spring_boot/controllers/CategoryController.java
@@ -1,16 +1,26 @@
 package dev.hyago.ecommerce_spring_boot.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.hyago.ecommerce_spring_boot.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryController {
 
   @RequestMapping(method=RequestMethod.GET)
-  public String list(){
-    return "REST está funcionando!";
+  public List<Category> list(){
+    Category cat1 = new Category(1, "Informática");
+    Category cat2 = new Category(2, "Escritório");
+    List<Category> list = new ArrayList<>();
+    list.add(cat1);
+    list.add(cat2);
+    return list;
   }
 
 }
