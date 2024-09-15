ce770cfc2a676d3d852f96ba3ba5d6c7c00feafb
Hyago Alves <dev.hyago@outlook.com>
2024-09-15 15:39:45 -0300

    refactor: addition of the H2 database and automatic creation of the category table

--- src/main/java/dev/hyago/ecommerce_spring_boot/domain/Category.java
+++ src/main/java/dev/hyago/ecommerce_spring_boot/domain/Category.java
@@ -2,10 +2,18 @@ package dev.hyago.ecommerce_spring_boot.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category implements Serializable{
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Integer id;
   private String name;
 
