2c876033f9ef8b5230f6a2366d1e9f3539c82396
Hyago Alves <dev.hyago@outlook.com>
2024-09-15 17:08:02 -0300

    filling initial data into the database

--- src/main/java/dev/hyago/ecommerce_spring_boot/EcommerceSpringBootApplication.java
+++ src/main/java/dev/hyago/ecommerce_spring_boot/EcommerceSpringBootApplication.java
@@ -1,13 +1,37 @@
 package dev.hyago.ecommerce_spring_boot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import dev.hyago.ecommerce_spring_boot.domain.Category;
+import dev.hyago.ecommerce_spring_boot.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class EcommerceSpringBootApplication {
+public class EcommerceSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ArrayList<String> categories = new ArrayList<>(Arrays.asList("Informática", "Escritório"));
+		for(int i = 0; i < categories.size(); i++){
+			Optional<Category> categoryExists = this.categoryRepository.findById(i + 1);
+			if(categoryExists.isEmpty()){
+				Category category = new Category(i + 1, categories.get(i));
+				categoryRepository.save(category);
+			}
+		}
 	}
 
 }
